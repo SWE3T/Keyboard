b28043229d04951ac6931d1363229e83332580be
Guilherme <guilherme.devon@hotmail.com>
2021-07-19 19:22:19 -0300

    Losing my mind already

--- src/sketch.js
+++ src/sketch.js
@@ -1,13 +1,22 @@
 function setup() {
+    context = new AudioContext();
     createCanvas(windowWidth, windowHeight);
+    osc = new p5.TriOsc();
+    env = new p5.Envelope();
+    osc.start();
+    osc.amp(0);
 }
 
 function draw() {
     if (mouseIsPressed) {
         fill(0);
+        context.resume().then(() => {
+            console.log('Playback resumed successfully');
+        });
     } else {
         fill(255);
     }
+
     ellipse(mouseX, mouseY, 80, 80);
 
     a = document.getElementById("a");
@@ -17,13 +26,13 @@ function draw() {
     k = document.getElementById("k");
     l = document.getElementById("l");
 
-
-
     if (keyIsDown(65)) {
         a.style.animation = "rise 1s infinite";
+        playNote(90);
     }
     else {
         a.style.webkitAnimationPlayState = "paused";
+        stopNote();
     }
 
     if (keyIsDown(83)) {
@@ -46,20 +55,44 @@ function draw() {
     else {
         j.style.webkitAnimationPlayState = "paused";
     }
-    
+
     if (keyIsDown(75)) {
         k.style.animation = "rise 1s infinite";
     }
     else {
         k.style.webkitAnimationPlayState = "paused";
     }
-    
+
     if (keyIsDown(76)) {
         l.style.animation = "rise 1s infinite";
     }
     else {
         l.style.webkitAnimationPlayState = "paused";
     }
+}
+
+// function keyPressed() {
+//     switch (keyCode) {
+//         case 65:
+//             playNote(30);
+//             console.log("a");
+//             break;
+//         case 83:
+//             playNote(45);
+//             break;
+//         case 68:
+//             playNote(60);
+//             break;
+//     }
+// }
+
+
+function playNote(note) {
+    osc.freq(note);
+}
+
+function stopNote() {
+    osc.fade(0, 0.5);
 }
 
 function windowResized() {
